534aebd1741ea544d006de3e23a54a9208a43e68
alvaro1981 <alvaro.alarcon.zamora@gmail.com>
2026-05-15 19:38:07 -0400

    a la columna hashed_password  de la tabla de usuarios la quite el index = true , no es recomendable porque no aporta en nada , agrega overhead en insert/updates

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -5,8 +5,8 @@ class Usuario(Base):
     __tablename__ = "usuarios"
 
     id = Column(Integer, primary_key=True, index=True)
-    email=Column(String(255), unique=True, nullable=False )
-    hashed_password = Column(String(255), nullable=False, index=True)
+    email=Column(String(255), unique=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
     username = Column(String(255), index=True)
     role = Column(String(255), default ="user",  index=True)
     is_active = Column(Boolean, default=True)
